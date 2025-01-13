681753936f997da998099a3140e210829b625286
paridhi-kapoor <paridhikapoor2004@gmail.com>
2025-01-13 23:31:22 +0530

    upload final file

--- virtual_assistant/commands.py
+++ virtual_assistant/commands.py
@@ -4,20 +4,27 @@ import random
 from datetime import datetime
 
 def parse_command(command):
-    if 'open browser' in command:
-        open_application('chrome')
-        return "Opening Browser."
     if 'open calculator' in command:
         open_application('calc')
         return "Opening Calculator."
-    if 'tell me a joke' in command:
+    elif 'open clock' in command:
+        open_application('start ms-clock:')
+        return "Opening Clock."
+    elif 'open camera' in command:
+        open_application('start microsoft.windows.camera:')
+        return "Opening Camera."
+    elif 'open calendar' in command:
+        open_application('start outlookcal:')
+        return "Opening Calendar."
+    elif 'tell me a joke' in command:
         return tell_joke()
-    if 'what time is it' in command:
+    elif 'what time is it' in command:
         return f"The time is {get_current_time()}."
-    if 'set reminder' in command:
-        return "Reminder feature coming soon!"
-    if 'exit' in command:
-        return "goodbye"
+    elif 'open notepad' in command:
+        open_application('notepad')
+        return "Opening Notepad."
+    elif 'exit' in command:
+        return "Goodbye!"
     else:
         return "I didn't understand that command."
 
@@ -25,12 +32,8 @@ def open_application(app_name):
     os.system(app_name)  # Runs the system app (e.g., calculator)
 
 def tell_joke():
-    jokes = [
-        "Why don't scientists trust atoms? Because they make up everything!",
-        "I told my computer I needed a break, and now it won’t stop sending me Kit-Kats."
-    ]
-    return random.choice(jokes)
-
+    jokes =  "Why don't scientists trust atoms? Because they make up everything!"
+    return jokes
 def get_current_time():
     now = datetime.now()
-    return now.strftime("%H:%M:%S")
+    return now.strftime("%I:%M %p")
